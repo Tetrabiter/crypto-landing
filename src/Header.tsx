718084ab6294fa7a0db1ef55b3c4335e12ba206e
Tetrabiter <noa.eskenkot@gmail.com>
2025-04-20 07:28:18 -0400

    initial commit

---
+++ src/Header.tsx
@@ -0,0 +1,17 @@
+const Header = () => {
+    return ( 
+        <header>
+            <img src="" alt="" />
+
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">Features</a></li>
+                <li><a href="">Career</a></li>
+            </ul>
+
+            <button>Launch App</button>
+        </header>
+    );
+}
+ 
+export default Header;
---
+++ src/Hero.tsx
@@ -0,0 +1,14 @@
+const Hero = () => {
+    return ( 
+        <main>
+            <p></p>
+            <p></p>
+            <div>
+                <button></button>
+                <button></button>
+            </div>
+        </main>
+    );
+}
+ 
+export default Hero;
